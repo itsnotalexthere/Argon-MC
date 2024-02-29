5428a02467fc3f22ce41d794bd1fd62e6cde4aca
alex89912 <105802970+alex89912@users.noreply.github.com>
2024-02-29 14:02:42 +0000

    Changes made to login page, seems to work now

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require("electron");
 
 // Eectron main function, DO NOT TOUCH UNLESS CHANGING SETTINGS
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
-  })
-  win.loadFile('index.html')
-}
+    height: 600,
+  });
+  win.menuBarVisible(false);
+  win.toggleTabBar();
+  win.loadFile("index.html");
+};
 
 app.whenReady().then(() => {
-  createWindow()
-})
+  createWindow();
+});
